17afc93d06349fc8b011d47677ec38c42f1062f2
Chung (Wesley) Tsang <chung.tsang@kcl.ac.uk>
2017-08-05 19:08:46 +0800

    update index.html

--- Source/App.js
+++ Source/App.js
@@ -1,1 +1,21 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+
+function maker() {
+    var lat = document.getElementById("lat").value
+    var lng = document.getElementById("lng").value
+
+    if (lat == "" || lng == "") {
+        alert("input boxes cannot be empty")
+    } else {
+        var pinBuilder = new Cesium.PinBuilder();
+ 
+        var bluePin = viewer.entities.add({
+            name : 'Blank blue pin',
+            position : Cesium.Cartesian3.fromDegrees(lng, lat),
+            billboard : {
+                image : pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
+                verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+            }
+        });
+    }
+}
