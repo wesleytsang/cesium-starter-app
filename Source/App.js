6d0d160f974ca6a6ed186159f04439252a2f08ca
Chung (Wesley) Tsang <chung.tsang@kcl.ac.uk>
2017-08-05 19:18:46 +0800

    update index.html

--- Source/App.js
+++ Source/App.js
@@ -1,4 +1,4 @@
-var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
 
 function maker() {
     var lat = document.getElementById("lat").value
